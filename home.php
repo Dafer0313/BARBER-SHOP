e144878e2b620e8d6e3f1705e241335b6618f77d
Dafer0313 <davidfernandocastillo@gmail.com>
2024-11-21 16:51:57 -0500

    home

--- home.php
+++ home.php
@@ -4,11 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
+    <link rel="stylesheet" href="css/home.css">
 </head>
 <body>
-    
-<h1>HOME</h1>
 
+    <header>
+        <a href="#" class="logo">
+            <img src="img/images.jpg" alt="logo acordeon" class="logo-img">
+            <h2 class="logo-nombre">BARBERIA VALLENATA</h2>
+        </a>
+
+        <nav>
+            <a href="">INICIO</a>
+            <a href="">RESERVAS</a>
+            <a href="">SOBRE NOSOTROS</a>
+            <a href="">CONTACTOS</a>
+        </nav>
+    </header>
+
+    <section>
+        <img src="img/diome.jpg" alt="">
+        <img src="img/martin.jpg" alt="">
+        <img src="img/binomio.jpg" alt="">
+        <img src="img/inquietos.jpg" alt="">
+        <img src="img/silvestre.jpg" alt="">
+        <img src="img/kaleth.jpg" alt="">
+    </section>
 
 </body>
 </html>
